013bec09e5b61e759a4d58028d94dd8fd4f47195
twinh <twinhuang@qq.com>
2018-12-06 22:10:02 +0800

    feat: 更新个人中心菜单

--- resources/views/navs/link.php
+++ resources/views/navs/link.php
@@ -11,31 +11,33 @@
 <!-- htmllint preset="none" -->
 <!-- htmllint tag-name-match="false" -->
 
-<?php foreach ($links as $i => $link) : ?>
-  <?php if ($link->isDivider()) : ?>
-    <li class="list-divider"></li>
-  <?php else : ?>
-    <li class="list-item-link">
-      <a class="js-link-nav-item list-item has-feedback" href="<?= $link->getUrl() ?>">
-        <div class="list-col list-col-left list-col-icon">
-          <?php if ($link['icon'] == 'image') : ?>
-            <img class="list-icon" src="<?= $link['image'] ?>">
-          <?php elseif ($link['icon'] == 'font') : ?>
-            <i class="list-icon <?= $link['font'] ?> <?= 'icon-bg-' . $i; ?>">
-            </i>
-          <?php else : ?>
-            <i class="list-icon iconfont <?= 'icon-bg-' . $i; ?>">
-              <?= $link['customFont'] ?>
-            </i>
-          <?php endif ?>
-        </div>
-        <div class="list-col list-middle">
-          <h4 class="list-heading">
-            <?= $link['name'] ?>
-          </h4>
-        </div>
-        <i class="bm-angle-right list-feedback"></i>
-      </a>
-    </li>
-  <?php endif ?>
-<?php endforeach ?>
+<ul class="list list-indented">
+  <?php foreach ($links as $i => $link) : ?>
+    <?php if ($link->isDivider()) : ?>
+      <li class="list-divider"></li>
+    <?php else : ?>
+      <li class="list-item-link">
+        <a class="js-link-nav-item list-item has-feedback" href="<?= $link->getUrl() ?>">
+          <div class="list-col list-col-left list-col-icon">
+            <?php if ($link['icon'] == 'image') : ?>
+              <img class="list-icon" src="<?= $link['image'] ?>">
+            <?php elseif ($link['icon'] == 'font') : ?>
+              <i class="list-icon <?= $link['font'] ?> <?= 'icon-bg-' . $i; ?>">
+              </i>
+            <?php else : ?>
+              <i class="list-icon iconfont <?= 'icon-bg-' . $i; ?>">
+                <?= $link['customFont'] ?>
+              </i>
+            <?php endif ?>
+          </div>
+          <div class="list-col list-middle">
+            <h4 class="list-heading">
+              <?= $link['name'] ?>
+            </h4>
+          </div>
+          <i class="bm-angle-right list-feedback"></i>
+        </a>
+      </li>
+    <?php endif ?>
+  <?php endforeach ?>
+</ul>
