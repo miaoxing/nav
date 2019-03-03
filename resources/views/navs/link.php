c2ea85fe9730a0034fb87edb51f875e73d4ee95b
twinh <twinhuang@qq.com>
2019-03-03 12:19:08 +0800

    refactoring: item内的内容加上 -item 区分 2

--- resources/views/navs/link.php
+++ resources/views/navs/link.php
@@ -31,7 +31,7 @@
             <?php endif ?>
           </div>
           <div class="list-col list-middle">
-            <h4 class="list-title">
+            <h4 class="list-item-title">
               <?= $link['name'] ?>
             </h4>
           </div>
